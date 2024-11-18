cb88cfe81ef30e4a390401dcd85eb123677f46d8
Radek Grzywacz <grzywaczra@gmail.com>
2024-11-18 23:00:40 +0100

    First designs

--- ColorsExtractorASM/Form1.Designer.cs
+++ ColorsExtractorASM/Form1.Designer.cs
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            openFileDialog1 = new OpenFileDialog();
+            image_loader_button = new Button();
+            pictureBox1 = new PictureBox();
+            trackBar1 = new TrackBar();
+            threads_label = new Label();
+            threads_number = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // image_loader_button
+            // 
+            image_loader_button.Location = new Point(348, 15);
+            image_loader_button.Name = "image_loader_button";
+            image_loader_button.Size = new Size(118, 51);
+            image_loader_button.TabIndex = 0;
+            image_loader_button.Text = "Load image";
+            image_loader_button.UseVisualStyleBackColor = true;
+            image_loader_button.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(244, 72);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(326, 307);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(29, 114);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(164, 45);
+            trackBar1.TabIndex = 2;
+            // 
+            // threads_label
+            // 
+            threads_label.AutoSize = true;
+            threads_label.Location = new Point(41, 97);
+            threads_label.Name = "threads_label";
+            threads_label.Size = new Size(107, 15);
+            threads_label.TabIndex = 3;
+            threads_label.Text = "Number of threads";
+            threads_label.Click += label1_Click;
+            // 
+            // threads_number
+            // 
+            threads_number.AutoSize = true;
+            threads_number.Location = new Point(102, 144);
+            threads_number.Name = "threads_number";
+            threads_number.Size = new Size(13, 15);
+            threads_number.TabIndex = 4;
+            threads_number.Text = "1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(threads_number);
+            Controls.Add(threads_label);
+            Controls.Add(trackBar1);
+            Controls.Add(pictureBox1);
+            Controls.Add(image_loader_button);
+            Name = "Form1";
+            Text = "Colors Extractor ASM";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button image_loader_button;
+        private PictureBox pictureBox1;
+        private TrackBar trackBar1;
+        private Label threads_label;
+        private Label threads_number;
     }
 }
--- ColorsExtractorASM/Form1.cs
+++ ColorsExtractorASM/Form1.cs
@@ -2,9 +2,54 @@ namespace ColorsExtractorASM
 {
     public partial class Form1 : Form
     {
+
+        private readonly int[] sliderValues = { 1, 2, 4, 8, 16, 32, 64 };
+
         public Form1()
         {
             InitializeComponent();
+
+            trackBar1.Minimum = 0; // First index
+            trackBar1.Maximum = sliderValues.Length - 1; // Last index
+            trackBar1.TickFrequency = 1;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+
+            // Attach event handler
+            trackBar1.Scroll += TrackBar1_Scroll;
+
+            // Set initial value
+            UpdateLabelWithValue();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName;
+            pictureBox1.Image = Image.FromFile(filePath);
+            pictureBox1.BorderStyle = BorderStyle.None;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            UpdateLabelWithValue();
+        }
+
+        private void UpdateLabelWithValue()
+        {
+            // Get the current value based on the trackBar1.Value index
+            int currentValue = sliderValues[trackBar1.Value];
+            threads_number.Text = $"Value: {currentValue}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
