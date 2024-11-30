3c6a02225f3de1eb58082ab568820d739ad45f2c
Radosław Grzywacz <radoslaw.grzywacz@softiq.pl>
2024-11-30 12:19:28 +0100

    cleaning git

--- ColorsExtractorASM/AnalysisResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ColorsExtractorASM
-{
-    public class AnalysisResult
-    {
-        public string Result { get; set; }
-        public TimeSpan ProcessingTime { get; set; }
-    }
-}
--- JAAsm/JAAsm.asm
+++
@@ -1,7 +0,0 @@
-﻿.code
-MyProc1 proc
-add RCX, RDX
-mov RAX, RCX
-ret
-MyProc1 endp
-end
